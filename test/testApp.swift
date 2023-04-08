ab966c21ec54cecdda8ed698600fc83f9181e802
ALex Antonica <alexandru.antonica@siteassist.co>
2023-04-08 12:13:20 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Alexandru Antonica on 08.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
